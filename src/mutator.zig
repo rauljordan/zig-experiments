5071586fa4323e9d3814f608ee20602462557950
rauljordan <raul@prysmaticlabs.com>
2023-03-25 22:55:33 -0400

    shrink expand

--- src/mutator.zig
+++ src/mutator.zig
@@ -6,9 +6,21 @@ pub fn main() !void {
     defer _ = gpa.deinit();
 
     var mutator = try Mutator.init(100, alloc);
-    var items = try mutator.ac.alloc(u8, 10);
-    defer mutator.ac.free(items);
-    _ = mutator.mutate(10, &items);
+    defer mutator.deinit();
+    var items = try mutator.ac.alloc(u8, 8);
+
+    // 8 bytes.
+    const initial = [8]u8{ 1, 2, 3, 4, 5, 6, 7, 8 };
+    std.mem.copy(u8, items, initial[0..8]);
+    std.debug.print("input 0x{x}\n", .{
+        std.fmt.fmtSliceHexLower(items),
+    });
+
+    mutator.input(items);
+    try mutator.mutate(3);
+    std.debug.print("output 0x{x}\n", .{
+        std.fmt.fmtSliceHexLower(mutator.output()),
+    });
 }
 
 pub const Strat = enum { Shrink, Expand };
@@ -16,7 +28,7 @@ pub const Strat = enum { Shrink, Expand };
 pub const Mutator = struct {
     const This = @This();
     ac: std.mem.Allocator,
-    input: []u8,
+    data: []u8,
     max_size: usize,
     seed: u64,
     rng: *std.rand.Xoshiro256,
@@ -29,7 +41,7 @@ pub const Mutator = struct {
     ) !This {
         var rand = std.rand.DefaultPrng.init(33);
         return .{
-            .input = &.{},
+            .data = &.{},
             .max_size = max_size,
             .ac = ac,
             .seed = 1000,
@@ -38,7 +50,19 @@ pub const Mutator = struct {
         };
     }
 
-    pub fn mutate(self: This, times: usize, input: *[]u8) void {
+    pub fn deinit(self: *This) void {
+        self.ac.free(self.data);
+    }
+
+    pub fn input(self: *This, b: []u8) void {
+        self.data = b;
+    }
+
+    pub fn output(self: This) []u8 {
+        return self.data;
+    }
+
+    pub fn mutate(self: *This, times: usize) !void {
         const strats: [2]Strat = [2]Strat{
             Strat.Shrink,
             Strat.Expand,
@@ -48,43 +72,70 @@ pub const Mutator = struct {
             const idx = self.rng.random().int(usize) % strats.len;
             const st = strats[idx];
             switch (st) {
-                .Shrink => self.shrink(input),
-                .Expand => self.expand(input),
+                .Shrink => try self.shrink(),
+                .Expand => try self.expand(),
             }
         }
     }
 
-    pub fn deinit(self: This) void {
-        _ = self;
-    }
-
     pub fn rand_offset(self: This) usize {
-        if (self.input.len == 0) {
+        if (self.data.len == 0) {
             return 0;
         }
-        return self.rng.random().int(usize) % self.input.len;
+        return self.rng.random().int(usize) % self.data.len;
     }
 
     pub fn seed(self: *This, new_seed: u64) void {
         self.seed = new_seed ^ 0x12640367f4b7ea35;
     }
 
-    fn shrink(self: This, data: *[]u8) void {
-        if (data.len == 0) {
+    fn shrink(self: *This) !void {
+        std.debug.print("shrinking\n", .{});
+        if (self.data.len == 0) {
             return;
         }
         const offset = self.rand_offset();
-        const to_remove = data.len - offset;
-        _ = to_remove;
-        std.debug.print("shrinking\n", .{});
+        const can_remove = self.data.len - offset;
 
-        // Drain some bytes from the input.
+        var max_remove: usize = can_remove;
+        if ((self.rng.random().int(usize) % 16) != 0) {
+            max_remove = @min(16, can_remove);
+        }
+        const to_remove = self.rng.random().int(usize) % max_remove;
+
+        // Drain the specified bytes from the input and put
+        // the retained results into a smaller buffer.
+        var smaller = try self.ac.alloc(u8, self.data.len - to_remove);
+        var i: usize = 0;
+        var j: usize = 0;
+        while (i < self.data.len) : (i += 1) {
+            if (i >= offset and i < offset + to_remove) {
+                continue;
+            } else {
+                smaller[j] = self.data[i];
+                j += 1;
+            }
+        }
+        self.ac.free(self.data);
+        self.input(smaller);
     }
 
-    fn expand(self: This, data: *[]u8) void {
-        _ = data;
-        _ = self;
+    fn expand(self: *This) !void {
         std.debug.print("expanding\n", .{});
+        if (self.data.len >= self.max_size) {
+            return;
+        }
+        const offset = self.rand_offset();
+        _ = offset;
+        var max_expand = self.max_size - self.data.len;
+
+        if ((self.rng.random().int(usize) % 16) != 0) {
+            max_expand = @min(16, max_expand);
+        }
+        const to_expand = self.rng.random().int(usize) % max_expand;
+        var expanded = try self.ac.alloc(u8, to_expand + self.data.len);
+        self.ac.free(self.data);
+        self.input(expanded);
     }
 
     fn bit(self: This, data: []u8) []u8 {
